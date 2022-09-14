aa7719a8aa9c1e6a6d6e421f628d93a2fbd26dc5
Heyonjee <guswl409@gmail.com>
2022-09-15 00:58:00 +0900

    solve

---
+++ Main01.java
@@ -0,0 +1,35 @@
+package test;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+
+		
+		
+		// Hello World!를 출력하시오.
+		
+		System.out.println("Hello World!");	
+		
+		
+		
+		
+		
+		
+		
+		
+		//Welcome�� ���� ���ó�� ����ϴ� ���α׷��� �ۼ��Ͻÿ�.
+				
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+				
+			
+						
+				
+				
+				
+
+		
+	}
+
+}
