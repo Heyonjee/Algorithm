95ad64169a8f71c7c8f313350d4b6786b67b6f87
Heyonjee <guswl409@gmail.com>
2022-09-16 23:38:10 +0900

    solve

---
+++ Main03.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.util.Scanner;
+
+public class Main03 {
+
+	public static void main(String[] args) {
+
+		
+		
+		/*
+		 * 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+		 * A가 B보다 큰 경우에는 '>'를 출력한다. 
+		 * A가 B보다 작은 경우에는 '<'를 출력한다.
+		 * A와 B가 같은 경우에는 '=='를 출력한다.
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		int a=0 ,b=0;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		
+		sc.close();
+	
+		
+		if(a>b) { 
+			System.out.println(">");
+		}else if(a<b) {
+			System.out.println("<");
+		}else {
+			System.out.println("==");
+		}
+	}
+	
+
+}
