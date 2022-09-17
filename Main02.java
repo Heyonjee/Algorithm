13405b9a7c3be321a50732a9c390b434510abb67
Heyonjee <guswl409@gmail.com>
2022-09-17 11:21:15 +0900

    solve

--- Main02.java
+++ Main02.java
@@ -22,18 +22,45 @@ public class Main02 {
 		
 		
 		/*
-		 * 두 정수 A와 B를 입력받은 다음, A+B , A-B, A/B 를 출력하는 프로그램을 작성하시오.
+		 * 두 정수 A와 B를 입력받은 다음, A+B , A-B, A/B, A*B 를 출력하는 프로그램을 작성하시오.
 		 * 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-		 * 첫째 줄에 A+B, A-B, A/B 를 출력한다.
+		 * 첫째 줄에 A+B, A-B, A/B, A*B 를 출력한다.
 		 */
 		
 		int A = 0;
 		int B = 0;
 		A = sc.nextInt();
 		B = sc.nextInt();
+		
+		
+		
 		System.out.println(A+B);
 		System.out.println(A-B);
 		System.out.println((double)A/B);
+		System.out.println(A*B);
+		
+		
+		/*
+		 * 두 자연수 C와 D가 주어진다. 이때, C+D, C-D, C*D, C/D(몫), C%D(나머지)를 출력하는 프로그램을 작성하시오.
+		 * 두 자연수 C와 D가 주어진다. (1 ≤ C, D ≤ 10,000)
+		 */
+		
+		int c,d=0;
+		c=sc.nextInt();
+		d=sc.nextInt();
+		
+		sc.close();
+		
+		System.out.println(c+d);
+		System.out.println(c-d);
+		System.out.println(c*d);
+		System.out.println(c/d);
+		System.out.println(c%d);
+	
+		
+		
+		
+		
 		
 	}
 
