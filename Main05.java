d8c819954dcf00726af1e31cc0767aa467fb6331
Heyonjee <guswl409@gmail.com>
2022-09-19 23:44:05 +0900

    solve

---
+++ Main05.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Main05 {
+
+	public static void main(String[] args) {
+		
+		//아래 예제와 같이 고양이를 출력하시오.
+		
+		//방법 1
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+		//방법 2 => 출력속도 더 빠름
+		System.out.println("\\    /\\\n" + " )  ( ')\n" + "(  /  )\n" + " \\(__)|");
+	}
+
+}
