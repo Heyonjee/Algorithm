1c31326b86f198e895ed31f7c43dd8725edf2ffc
Heyonjee <guswl409@gmail.com>
2022-09-20 23:40:04 +0900

    solve

--- Main05.java
+++ Main05.java
@@ -75,10 +75,60 @@ public class Main05 {
 		b = sc.nextInt();
 		c = sc.nextInt();
 		
-		sc.close();
+		
 		
 		System.out.println(b*(c%10)+"\n"+b*((c/10)%10)+"\n"+b*(c/100));
 		System.out.println(b*c);
+		
+		
+		
+		
+		
+		
+		/*
+		 * (d+e)%f는 ((d%e) + (e%f))%f 와 같을까? (d×e)%f는 ((d%f) × (e%f))%f 와 같을까?
+		 * 세 수 d, e, f가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+		 */
+		
+		int d, e, f = 0;
+		d=sc.nextInt();
+		e=sc.nextInt();
+		f=sc.nextInt();
+		
+		
+		
+		System.out.println((d+e)%f+"\n"+((d%f) + (e%f))%f+"\n"+(d*e)%f+"\n"+((d%f)*(e%f))%f );
+		
+		
+		
+		
+		
+		
+		/*
+		 * 동혁이는 오래된 창고를 뒤지다가 낡은 체스판과 피스를 발견했다. 체스판의 먼지를 털어내고 걸레로 닦으니 그럭저럭 쓸만한 체스판이 되었다.
+		 * 하지만, 검정색 피스는 모두 있었으나, 흰색 피스는 개수가 올바르지 않았다.
+		 * 체스는 총 16개의 피스를 사용하며, 킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개로 구성되어 있다. 
+		 * 동혁이가 발견한 흰색 피스의 개수가 주어졌을 때, 몇 개를 더하거나 빼야 올바른 세트가 되는지 구하는 프로그램을 작성하시오.
+		 */
+		
+		int king = sc.nextInt();
+		int queen = sc.nextInt();
+		int rook = sc.nextInt();
+		int bishop = sc.nextInt();
+		int knight = sc.nextInt();
+		int pawn = sc.nextInt();
+		
+		int result1 = 1-king;
+		int result2 = 1-queen;
+		int result3 = 2-rook;
+		int result4 = 2-bishop;
+		int result5 = 2-knight;
+		int result6 = 8-pawn;
+		
+		sc.close();
+		
+		System.out.println(result1+" "+result2+" "+result3+" "+result4+" "+result5+" "+result6);
+		
 		
 		
 		
