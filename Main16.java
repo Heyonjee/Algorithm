763bc83c052a35e6fe76c4137e9b85bf21ef7c08
Heyonjee <guswl409@gmail.com>
2022-10-22 15:00:57 +0900

    배열사용하기 연산시 자료형에 주의!

--- Main16.java
+++ Main16.java
@@ -52,7 +52,41 @@ public class Main16 {
 		
 		
 		
+		/*<문제>
+		 * 첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+		 *둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+		 *각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+		 */
+		
+			int c= sc.nextInt();  //테스트 케이스 
+		             
+			for(int i=0; i<c; i++) {
+			
+			int s =sc.nextInt();  //학생수 
+			double[] arr2= new double[s];
+			
+			double sum=0;  //성적누적 합 :double형
+			
+			for(int j=0; j<s; j++) {
+				int score= sc.nextInt(); //성적입력
+				arr2[j]=score;
+				sum=sum+score;  //성적누적 합
+
+			}
+			
+			double avg = (sum/s);
+			double count=0; //평균 넘는 학생 수  =>double로 받는 이유: 비율계산시 자료형 통일
+			
+			for(int j=0; j<s; j++) {
+				if(arr2[j]>avg) { //평균 넘는 학생 수 
+					count++;
+				}
+			}
+			
+			System.out.printf("%.3f%%\n", ((count/s)*100));   //%.3f => 소수점 셋째자리까지 출력!  %% => "%" 문자를 출력
+		
 		
 	}
 
+}
 }
