c54597f5f35a4ace23676882d108bb7d1d3ce2e7
Heyonjee <guswl409@gmail.com>
2022-12-09 00:00:50 +0900

    출력문제

--- Main57.java
+++ Main57.java
@@ -34,6 +34,30 @@ public class Main57 {
 		System.out.println("88 8888 88 88   88  88888    8888");
 		System.out.println(" 888  888 888888888 88  88      88");
 		System.out.println("  88  88  88     88 88   88888888");
+		
+		
+		
+		
+		/*
+		 * <문제>
+		 * 스타워즈에 등장하는 로봇인 C3PO는 요즘 콜센터에 근무하고 있다.
+		 * 콜센터에 앉아있는 C3PO를 그리는 프로그램을 작성하시오.
+		 */
+		
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		
+		
+		
 	}
+	
 
 }
