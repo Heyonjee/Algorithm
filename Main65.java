f98e0b1433df4cb1b3f1d09420c16db2d5edac8d
Heyonjee <guswl409@gmail.com>
2022-12-12 18:08:38 +0900

    출력문제 solve

---
+++ Main65.java
@@ -0,0 +1,29 @@
+package test;
+
+public class Main65 {
+
+	public static void main(String[] args) {
+
+		
+		/*
+		 * <문제>
+		 * 동혁이를 위해 NFC 서부와 북부 디비전 순위를 출력하는 프로그램을 작성하시오.
+		 * NFC 서부와 북부 디비전 순위를 출력한다.
+		 */
+		
+		System.out.println("NFC West       W   L  T");
+		System.out.println("-----------------------");
+		System.out.println("Seattle        13  3  0");
+		System.out.println("San Francisco  12  4  0");
+		System.out.println("Arizona        10  6  0");
+		System.out.println("St. Louis      7   9  0");
+		System.out.println();
+		System.out.println("NFC North      W   L  T");
+		System.out.println("-----------------------");
+		System.out.println("Green Bay      8   7  1");
+		System.out.println("Chicago        8   8  0");
+		System.out.println("Detroit        7   9  0");
+		System.out.println("Minnesota      5  10  1");
+	}
+
+}
