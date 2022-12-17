bc9c672fb351803e63a44410d740f4765a4f0cdd
Heyonjee <guswl409@gmail.com>
2022-12-17 11:21:37 +0900

    BigInteger 사용

--- Main67.java
+++ Main67.java
@@ -1,11 +1,35 @@
 package test;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main67 {
 
 	public static void main(String[] args) {
 
+		
+		
+		
+		/*
+		 * <문제>
+		 * 두 정수 A와 B가 주어졌을 때, 두 수의 곱을 출력하는 프로그램을 작성하시오.
+		 * 첫째 줄에 정수 A와 B가 주어진다. 두 정수는 0보다 크거나 같은 정수이며, 0을 제외한 정수는 0으로 시작하지 않으며, 수의 앞에
+		 * 불필요한 0이 있는 경우도 없다. 또한, 수의 길이는 300,000자리를 넘지 않는다.
+		 * 두 수의 곱을 출력한다.
+		 */
+		
+		Scanner sc2 = new Scanner(System.in); //스캐너 선언
+		
+		BigInteger a= sc2.nextBigInteger(); //큰 수이기 때문에 BigInteger사용
+		BigInteger b= sc2.nextBigInteger();
+		
+		System.out.println(a.multiply(b)); //곱하기
+		
+		
+		
+		
+
+		
 		
 		/*
 		 * <문제>
